3b5acea84ab18b3be8dbd78360fed5ad59071ee3
prabhakar <prabhakarsingh3175@gamil.com>
2021-04-05 14:57:31 +0530

    add navbar file

--- PageObjectModel/NavbarMenu.py
+++ PageObjectModel/NavbarMenu.py
@@ -1,3 +1,13 @@
 class NavbarMenu:
     #catalog_xpath="//a[@class='nav-link']//p[@xpath=1][contains(text(),'Catalog')]"
-    catalog_xpath="/html/body/div[3]/aside/div/div[4]/div/div/nav/ul/li[2]/a"
+    catalog_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-book']"
+    sales_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-shopping-cart']"
+    customer_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon far fa-user']"
+    promotions_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-tags']"
+    content_management_xpth="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-cubes']"
+    configuration_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-cogs']"
+    system_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-cube']"
+    reports_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-chart-line']"
+    help_xpath="//nav[@class='mt-2']//ul[@role='menu']//li//a//i[@class='nav-icon fas fa-question-circle']"
+
+    #all_menu_option_xpath="//nav[@class='mt-2']//ul[@role='menu']//li[@class='nav-item has-treeview']"
--- TestCases/test_navbar_menu.py
+++ TestCases/test_navbar_menu.py
@@ -1,4 +1,5 @@
 from TestData import config
+import time
 from PageObjectModel.NavbarMenu import NavbarMenu
 from PageObjectModel.Login import LoginPOM
 class TestNavbarMenu:
@@ -9,6 +10,50 @@ class TestNavbarMenu:
         driver.maximize_window()
         lg=LoginPOM()
         lg.login(driver)
+        #1
         driver.find_element_by_xpath(NavbarMenu.catalog_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.catalog_xpath).click()
+        #2
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.sales_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.sales_xpath).click()
+        #3
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.customer_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.customer_xpath).click()
+        #4
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.promotions_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.promotions_xpath).click()
+        #5
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.content_management_xpth).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.content_management_xpth).click()
+        #6
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.configuration_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.configuration_xpath).click()
+        #7
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.system_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.system_xpath).click()
+        #8
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.reports_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.reports_xpath).click()
+        #9
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.help_xpath).click()
+        time.sleep(1)
+        driver.find_element_by_xpath(NavbarMenu.help_xpath).click()
+
 
 
